9b5b90c6b8faa9cee0dfaaf1fb4c6ed739157bdd
chenjiabo <chen_wangyi666@163.com>
2024-11-16 18:05:38 +0800

    完成装备与材料的储存，修复无enemy时冲刺报空指针异常的错误(在报错代码前加判断，若为null，则返回)

--- Assets/scripts/Controllers/SkillControllers/CrystalSkillController.cs
+++ Assets/scripts/Controllers/SkillControllers/CrystalSkillController.cs
@@ -50,6 +50,10 @@ public class CrystalSkillController : MonoBehaviour
 
         if(canMove)
         {
+            if (closestEnemy == null)
+            {
+                return;
+            }
             transform.position = Vector2.MoveTowards(transform.position, closestEnemy.position, moveSpeed * Time.deltaTime);
 
             if(Vector2.Distance(transform.position, closestEnemy.position) < 1)
--- Assets/scripts/Items and Inventory/Inventory.cs
+++ Assets/scripts/Items and Inventory/Inventory.cs
@@ -7,13 +7,21 @@ public class Inventory : MonoBehaviour
 
     public static Inventory instance;
     // 记录所有的inventoryItems，下面的字典用于判断ItemData是否在list中
-    public List<InventoryItem> inventoryItems;
+    // inventory中储存equipment
+    public List<InventoryItem> inventory;
     // 用于判断ItemData是否已加入到inventoryItems中
     public Dictionary<ItemData, InventoryItem> inventoryDictionary;
 
+    // stash中储存material
+    public List<InventoryItem> stash;
+    public Dictionary <ItemData, InventoryItem> stashDictionary;
+
     [Header("Inventory UI")]
+    // 展示在屏幕上的inventorySlot对象的父对象，主要用于确认inventorySlot对象的渲染位置，获取inventorySlot对象
     [SerializeField] private Transform inventorySlotParent;
-    private UI_ItemSlot[] itemSlot;
+    [SerializeField] private Transform stashSlotParent;
+    private UI_ItemSlot[] inventoryItemSlot;
+    private UI_ItemSlot[] stashItemSlot;
 
     private void Awake()
     {
@@ -29,21 +37,44 @@ public class Inventory : MonoBehaviour
 
     private void Start()
     {
-        inventoryItems = new List<InventoryItem>(); 
+        inventory = new List<InventoryItem>(); 
         inventoryDictionary = new Dictionary<ItemData, InventoryItem>();
 
-        itemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
+        stash = new List<InventoryItem>();
+        stashDictionary = new Dictionary<ItemData, InventoryItem>();
+
+        inventoryItemSlot = inventorySlotParent.GetComponentsInChildren<UI_ItemSlot>();
+        stashItemSlot = stashSlotParent.GetComponentsInChildren<UI_ItemSlot>();
     }
 
     private void UpdateSlotUI()
     {
-        for (int i = 0; i < inventoryItems.Count; i++)
+        // 渲染inventoryItems列表中的对象
+        for (int i = 0; i < inventory.Count; i++)
         {
-            itemSlot[i].UpdateSlot(inventoryItems[i]);
+            inventoryItemSlot[i].UpdateSlot(inventory[i]);
+        }
+        for(int i = 0; i < stash.Count; i++)
+        {
+            stashItemSlot[i].UpdateSlot(stash[i]);
         }
     }
 
     public void AddItem(ItemData _item)
+    {
+        if(_item.itemType == ItemType.Equipment)
+        {
+            AddToInventory(_item);
+        }
+        else if(_item.itemType == ItemType.Material)
+        {
+            AddToStash(_item);
+        }
+
+        UpdateSlotUI();
+    }
+
+    private void AddToInventory(ItemData _item)
     {
         // 从字典中获取与指定键相关联的值。如果找到该键，则TryGetValue方法会返回一个布尔值true，并通过out参数返回该键对应的值；
         // 如果没有找到该键，则返回false，并且out参数会被设置为该类型的默认值
@@ -54,11 +85,23 @@ public class Inventory : MonoBehaviour
         else
         {
             InventoryItem newItem = new InventoryItem(_item);
-            inventoryItems.Add(newItem);
+            inventory.Add(newItem);
             inventoryDictionary.Add(_item, newItem);
         }
+    }
 
-        UpdateSlotUI();
+    private void AddToStash(ItemData _item)
+    {
+        if (stashDictionary.TryGetValue(_item, out InventoryItem value))
+        {
+            value.AddStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(_item);
+            stash.Add(newItem);
+            stashDictionary.Add(_item, newItem);
+        }
     }
 
     public void RemoveItem(ItemData _item)
@@ -67,12 +110,25 @@ public class Inventory : MonoBehaviour
         {
             if(value.stackSize <= 1)
             {
-                inventoryItems.Remove(value);
+                inventory.Remove(value);
                 inventoryDictionary.Remove(_item);
             }
             else
             {
                 value.RemoveStack();
+            }
+        }
+
+        if (stashDictionary.TryGetValue(_item, out InventoryItem stashValue))
+        {
+            if (stashValue.stackSize <= 1)
+            {
+                stash.Remove(stashValue);
+                stashDictionary.Remove(_item);
+            }
+            else
+            {
+                stashValue.RemoveStack();
             }
         }
 
